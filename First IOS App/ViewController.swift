43bce4eb923cda0487c2dee2f5aca52593364947
frank.gitonga <frank.gitonga@incentrocom>
2018-11-27 13:49:52 +0300

    Initial commit

---
+++ First IOS App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First IOS App
+//
+//  Created by Frankline Mutwiri on 11/8/18.
+//  Copyright © 2018 Qazana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
